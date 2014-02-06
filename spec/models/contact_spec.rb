925e09183c1f266f39fafcd0d80983aeaabd1ca9
Michael Pope <amorphid@captaincoder.io>
2014-02-05 21:34:37 -0800

    Add test for validation of last_name to Contact model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -16,7 +16,12 @@ describe Contact do
       expect(subject).to have(0).errors_on(:first_name)
     end
 
-    it "presene of last name"
+    it "presene of last name" do
+      expect(subject).to have(1).errors_on(:last_name)
+      subject.last_name = Faker::Name.first_name
+      expect(subject).to have(0).errors_on(:last_name)
+    end
+
     it "uniqueness of email"
   end
 
