fc388aaaa511f9bb85fb321bca5a0e6aa0a0e367
Michael Pope <amorphid@captaincoder.io>
2014-02-05 21:04:45 -0800

    Add email column to contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206050237) do
+ActiveRecord::Schema.define(version: 20140206050407) do
 
   create_table "contacts", force: true do |t|
+    t.string "email"
   end
 
 end
