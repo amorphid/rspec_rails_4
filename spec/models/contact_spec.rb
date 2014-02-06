74fd6c60c3fe96eadf52e1c6d2b658c6006670a8
Michael Pope <amorphid@captaincoder.io>
2014-02-05 21:44:07 -0800

    Update Contact model tests to use fabrication gem for validations

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,25 +1,25 @@
 require "spec_helper"
 
 describe Contact do
-  subject { Contact.new }
+  subject { Fabricate.build(:contact) }
 
   context "validates" do
     it "presene of email" do
-      expect(subject).to have(1).errors_on(:email)
-      subject.email = Faker::Internet.email
       expect(subject).to have(0).errors_on(:email)
+      subject.email = nil
+      expect(subject).to have(1).errors_on(:email)
     end
 
     it "presene of first name" do
-      expect(subject).to have(1).errors_on(:first_name)
-      subject.first_name = Faker::Name.first_name
       expect(subject).to have(0).errors_on(:first_name)
+      subject.first_name = nil
+      expect(subject).to have(1).errors_on(:first_name)
     end
 
     it "presene of last name" do
-      expect(subject).to have(1).errors_on(:last_name)
-      subject.last_name = Faker::Name.first_name
       expect(subject).to have(0).errors_on(:last_name)
+      subject.last_name = nil
+      expect(subject).to have(1).errors_on(:last_name)
     end
 
     it "uniqueness of email"
