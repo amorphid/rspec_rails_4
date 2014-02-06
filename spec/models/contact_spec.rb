3cee09d773a76dceb87a1fa8d91a14dfdafd910b
Michael Pope <amorphid@captaincoder.io>
2014-02-05 21:09:13 -0800

    Add validation for presence of email in Contact model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,8 +1,14 @@
 require "spec_helper"
 
 describe Contact do
+  subject { Contact.new }
+
   context "validates" do
-    it "presene of email"
+    it "presene of email" do
+      expect(subject).to be_invalid
+      subject.email = "bob@example.com"
+      expect(subject).to be_valid
+    end
     it "presene of first name"
     it "presene of last name"
     it "uniqueness of email"
