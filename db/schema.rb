7b3104ead711d013a2d69a040a1f675923ab3041
Michael Pope <amorphid@captaincoder.io>
2014-02-05 21:27:55 -0800

    Add last name column to contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206051524) do
+ActiveRecord::Schema.define(version: 20140206052634) do
 
   create_table "contacts", force: true do |t|
     t.string "email"
     t.string "first_name"
+    t.string "last_name"
   end
 
 end
