6265b948dc80348b0f424ec6a746f23197f5cf45
Michael Pope <amorphid@captaincoder.io>
2014-02-05 22:16:33 -0800

    Contact#full_name returns first name + last name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,10 @@ class Contact < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    first_name = self.first_name
+    last_name  = self.last_name
+    first_name + " " + last_name
+  end
 end
